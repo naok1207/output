060b197f3621570a2ac062b2a5f9f7d8d3034047
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-12-02 07:37:31 +0900

    :sparkles: Add i18n国際化 設定

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Output
       g.helper false
     end
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
